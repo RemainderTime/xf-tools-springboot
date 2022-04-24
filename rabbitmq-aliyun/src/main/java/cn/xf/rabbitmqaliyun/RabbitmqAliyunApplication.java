a0e456f03c167345c32db8adf48992cee0bd2e44
RemainderTime <remaindertime@gmail.com>
2022-04-24 20:42:38 +0800

    springboot 整合 阿里云消息队列 RabbitMQ 版 服务

---
+++ rabbitmq-aliyun/src/main/java/cn/xf/rabbitmqaliyun/RabbitmqAliyunApplication.java
@@ -0,0 +1,13 @@
+package cn.xf.rabbitmqaliyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAliyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqAliyunApplication.class, args);
+    }
+
+}
